de7081b1c2c613e0668fe15b8e8c912162468c4b
F8THEM8 <faithfebisdesign@gmail.com>
2024-12-12 00:54:21 +0100

    Update functions.php

--- custom_app/inc/functions.php
+++ custom_app/inc/functions.php
@@ -1,28 +1,64 @@
 <?php
-function shopify_call($token, $shop, $api_endpoint, $array = array(), $method = 'GET') {
-    $url = "https://" . $shop . ".myshopify.com" . $api_endpoint;
+
+function shopify_call($token, $shop, $api_endpoint, $query = array(), $method = 'GET', $request_headers = array()) {
     
-    // Initialize cURL session
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// Build URL
+	$url = "https://" . $shop . ".myshopify.com" . $api_endpoint;
+	if (!is_null($query) && in_array($method, array('GET', 	'DELETE'))) $url = $url . "?" . http_build_query($query);
+
+	// Configure cURL
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_HEADER, TRUE);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+	curl_setopt($curl, CURLOPT_MAXREDIRS, 3);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+	// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 3);
+	// curl_setopt($curl, CURLOPT_SSLVERSION, 3);
+	curl_setopt($curl, CURLOPT_USERAGENT, 'My New Shopify App v.1');
+	curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+	curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+
+	// Setup headers
+	$request_headers[] = "";
+	if (!is_null($token)) $request_headers[] = "X-Shopify-Access-Token: " . $token;
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $request_headers);
+
+	if ($method != 'GET' && in_array($method, array('POST', 'PUT'))) {
+		if (is_array($query)) $query = http_build_query($query);
+		curl_setopt ($curl, CURLOPT_POSTFIELDS, $query);
+	}
     
-    // Set the headers for the request
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        "X-Shopify-Access-Token: " . $token
-    ));
+	// Send request to Shopify and capture any errors
+	$response = curl_exec($curl);
+	$error_number = curl_errno($curl);
+	$error_message = curl_error($curl);
 
-    // Set the request method and parameters
-    if ($method == 'POST') {
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($array));
-    }
+	// Close cURL to be nice
+	curl_close($curl);
 
-    // Execute the cURL request and capture the response
-    $response = curl_exec($ch);
-    curl_close($ch);
+	// Return an error is cURL has a problem
+	if ($error_number) {
+		return $error_message;
+	} else {
 
-    // Return the response
-    return json_decode($response, true);
+		// No error, return Shopify's response by parsing out the body and the headers
+		$response = preg_split("/\r\n\r\n|\n\n|\r\r/", $response, 2);
+
+		// Convert headers into an array
+		$headers = array();
+		$header_data = explode("\n",$response[0]);
+		$headers['status'] = $header_data[0]; // Does not contain a key, have to explicitly set
+		array_shift($header_data); // Remove status, we've already set it above
+		foreach($header_data as $part) {
+			$h = explode(":", $part);
+			$headers[trim($h[0])] = trim($h[1]);
+		}
+
+		// Return headers and Shopify's response
+		return array('headers' => $headers, 'response' => $response[1]);
+
+	}
+    
 }
-?>
