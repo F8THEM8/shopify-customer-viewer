01c11e7dba61447d7ff2685765dd16a4ca4c5a1b
F8THEM8 <faithfebisdesign@gmail.com>
2024-12-11 23:14:30 +0100

    Update install.php

--- custom_app/install.php
+++ custom_app/install.php
@@ -1,11 +1,17 @@
 <?php
+// Load environment variables
+require_once 'vendor/autoload.php'; // Ensure Composer's autoloader is included
+Dotenv\Dotenv::createImmutable(__DIR__)->load(); // Load .env file
+
 $shop = $_GET['shop'];
-$api_key = "d634765af60561d695affc6d1d2f5981";
-$scopes = "read_orders,read_customer";
+$api_key = getenv('API_KEY'); // Retrieve the API key from the .env file
+$scopes = "read_orders,read_customers";
 $redirect_uri = "http://github.COM/F8THEM8/shopify-customer-viewer/custom_app/generate_token.php";
 
+// Build the installation URL
 $install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 
+// Redirect to the install URL
 header("Location: " . $install_url);
 die();
 ?>
