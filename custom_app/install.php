0a6ccc281274a81581901e615a431f1038d01bcc
F8THEM8 <faithfebisdesign@gmail.com>
2024-12-11 22:14:12 +0100

    Update install.php

--- custom_app/install.php
+++ custom_app/install.php
@@ -1,7 +1,7 @@
 <?php
 $shop = $_GET['shop'];
 $api_key = "d634765af60561d695affc6d1d2f5981";
-$scopes = "read_orders,write_products";
+$scopes = "read_orders,read_customer";
 $redirect_uri = "http://github.COM/F8THEM8/shopify-customer-viewer/custom_app/generate_token.php";
 
 $install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
